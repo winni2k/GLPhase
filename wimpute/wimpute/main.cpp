f577d9781697b5c18514476ac0494d2dfec1ddee
Warren W. Kretzschmar <wkretzsch@gmail.com>
2013-04-19 22:09:12 +0100

    moved wimpute files one dir lower

---
+++ wimpute/wimpute/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  wimpute
+//
+//  Created by winni on 19/04/2013.
+//  Copyright (c) 2013 warren kretzschmar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
