983c716ebb59aaea9bb6f35519cb75db5dc2bc71
Warren W. Kretzschmar <wkretzsch@gmail.com>
2013-04-19 23:03:09 +0100

    made some changes...

---
+++ wimpute/wimpute/add.h
@@ -0,0 +1,10 @@
+/* @(#)add.h
+ */
+
+#ifndef _ADD_H
+#define _ADD_H 1
+
+int add(int x, int y); // function prototype for add.h
+
+#endif /* _ADD_H */
+
--- wimpute/wimpute/main.cpp
+++ wimpute/wimpute/main.cpp
@@ -6,13 +6,27 @@
 //  Copyright (c) 2013 warren kretzschmar. All rights reserved.
 //
 
+// my first program in C++
+
 #include <iostream>
+#include "add.h" // this brings in the declaration for add()
+using namespace std;
+
+int add (int x, int y);
 
 int main(int argc, const char * argv[])
 {
+    cout << "Hello World! " << add(1,2) << endl;
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "bool:\t\t" << sizeof(bool) << " bytes" << endl;
+    cout << "char:\t\t" << sizeof(char) << " bytes" << endl;
+    cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << endl;
+    cout << "short:\t\t" << sizeof(short) << " bytes" << endl;
+    cout << "int:\t\t" << sizeof(int) << " bytes" << endl;
+    cout << "long:\t\t" << sizeof(long) << " bytes" << endl;
+    cout << "float:\t\t" << sizeof(float) << " bytes" << endl;
+    cout << "double:\t\t" << sizeof(double) << " bytes" << endl;
+    cout << "long double:\t" << sizeof(long double) << " bytes" << endl;
+
     return 0;
 }
-
