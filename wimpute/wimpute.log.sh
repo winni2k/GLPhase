eaa741f7f8c75c5703460a8bf1cdedacfcccbfa3
kretzsch <wkretzsch@gmail.com>
2013-06-24 15:02:13 +0100

    added log on fenghuang

---
+++ wimpute/wimpute.log.sh
@@ -0,0 +1,11 @@
+### log of work I've done on wimpute
+
+###########
+# Sat Jun 01 16:52:48 BST 2013
+# created a dir in which to summarize all the previous runs into one
+mkdir results/2013-06-01_create-int-plot
+
+# started work on integrated plot
+touch results/2013-06-01_create-int-plot/create-int-plot.pl
+
+# renamed some dirs in results to easier understand what the experiments were
