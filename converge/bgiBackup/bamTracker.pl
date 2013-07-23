f839b27f6fef735b5c8eeb9ec8d7523bab86a3d0
Warren W. Kretzschmar <wkretzsch@gmail.com>
2013-07-23 19:06:15 +0100

    git-annex automatic sync

--- converge/bgiBackup/bamTracker.pl
+++ converge/bgiBackup/bamTracker.pl
@@ -14,36 +14,67 @@ use ParseArgs 0.000002 qw/getCommandArguments/;
 use POSIX;
 use DBI;
 use bamTrackLib;
+use Carp;
+use DM;
 
 my %args = getCommandArguments(
     requiredArgs => {
 
-        WD       => undef,
+        WD => undef,
 
-                     # database options
-        host     => 'mus.well.ox.ac.uk',
-        db       => "bgibams",
-        user     => 'winni',
-        
-                    # diagnosis
-                     listDrivers=> 0,
+        # database options
+        host => 'mus.well.ox.ac.uk',
+        db   => "bgibams",
+        user => 'winni',
 
-                     # commands
-                     addBamList=>0,
-                     removeBamList=>0,
-                     validateBamList=>0,
+        # diagnosis
+        listDrivers => 0,
+
+        # commands
+        addBamList       => 0,
+        removeBamList    => 0,
+        validateBamList  => 0,
+        backupBamList    => 0,
+        backupTargetDir  => 0,
+        backupDeviceName => 0,
+
+        # validation jar file
+        validateSamJar => 0,
+
+        # DM related commands
+        dryRun  => 1,
+        numJobs => 1,
     }
 );
 
-my $bto = bamTrackLib->new( db=>$args{db}, host=>$args{host}, user=>$args{user});
+my $dm = DM->new( dryRun => $args{dryRun}, numJobs => $args{numJobs} );
 
-$bto->registerBams(fileList => $args{addBamList}) if $args{addBamList};
-$bto->dropBams(fileList=> $args{removeBamList}) if $args{removeBamList};
-$bto->validateBams(fileList => $args{validateBamList}) if $args{validateBamList};
+my $bto =
+  bamTrackLib->new( db => $args{db}, host => $args{host}, user => $args{user} );
+
+$bto->registerBams( fileList => $args{addBamList} ) if $args{addBamList};
+$bto->dropBams( fileList => $args{removeBamList} ) if $args{removeBamList};
+$bto->validateBams( fileList => $args{validateBamList} )
+  if $args{validateBamList};
 $bto->listDrivers() if $args{listDrivers};
 
+if ( $args{backupBamList} ) {
+    croak "need to specify backupTargetDir if backing up"
+      unless $args{backupTargetDir};
+    croak "need to specify backupDeviceName if backing up"
+      unless $args{backupDeviceName};
+    croak "need to specify validateSamJar if backing up"
+      unless $args{validateSamJar};
 
+    # register bams to be backed up
+    $bto->backupBams(
+        fileList         => $args{backupBamList},
+        backupTargetDir  => $args{backupTargetDir},
+        backupDeviceName => $args{backupDeviceName},
+        validateSamJar   => $args{validateSamJar},
+    );
 
+}
 
 __END__
 
