14e5fd7b05e2185ffbf8b635c55724945d43f7db
Warren W. Kretzschmar <wkretzsch@gmail.com>
2013-09-11 17:26:13 +0100

    git-annex automatic sync

--- insti/src/testing/insti_unittest1.cpp
+++ insti/src/testing/insti_unittest1.cpp
@@ -78,7 +78,8 @@ TEST(Insti, loadHaps){
 //    cerr << "BLOINC1\n";
     ASSERT_EXIT(lp.load_refPanel( "", sampleHaps), ::testing::ExitedWithCode(1),"Need to define a legend file if defining a reference haplotypes file");
     ASSERT_EXIT(lp.load_refPanel( sampleLegend, ""), ::testing::ExitedWithCode(1),"Need to define a reference haplotypes file if defining a legend file");
-    
+
+    cerr<<sampleLegend <<endl;
     lp.load_refPanel( sampleLegend, sampleHaps);
 
 //    cerr << "BLOINC2\n";
