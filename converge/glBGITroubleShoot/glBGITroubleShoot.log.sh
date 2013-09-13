14df2d619e70054747db6107ba2988a6ce7cb199
Warren Kretzschmar <wkretzsch@gmail.com>
2013-09-13 11:19:38 +0100

    git-annex automatic sync

---
+++ converge/glBGITroubleShoot/glBGITroubleShoot.log.sh
@@ -0,0 +1,15 @@
+###########
+# Fri Sep 13 11:10:16 BST 2013
+# trying to figure out if gls on the BGI side are ok
+
+# pull out random site from chr20 in what I think is a good vcf
+# got site from ~/rare_snps/lists/bgi_genotyping.chr20.sites.tsv
+# 20:350490
+
+# file exists and looks ok
+vcftools  --gzvcf ~/rare_snps/shared/GLs/9300/chr20.prob.vcf.gz
+
+# pull out site of interest 
+tabix -h ~/rare_snps/shared/GLs/9300/chr20.prob.vcf.gz 20:350489-350490 > chr20.350490.9300.vcf
+
+
