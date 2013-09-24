f6b70a952702fe1aae49d255399f3e7441ce5dca
Warren Kretzschmar <wkretzsch@gmail.com>
2013-09-24 11:43:00 +0100

    git-annex automatic sync
    Former-commit-id: 9ed471745b50672807be1bbb142d424c56e9d09a

--- converge/glBGITroubleShoot/glBGITroubleShoot.log.sh
+++ converge/glBGITroubleShoot/glBGITroubleShoot.log.sh
@@ -12,4 +12,7 @@ vcftools  --gzvcf ~/rare_snps/shared/GLs/9300/chr20.prob.vcf.gz
 # pull out site of interest 
 tabix -h ~/rare_snps/shared/GLs/9300/chr20.prob.vcf.gz 20:350489-350490 > chr20.350490.9300.vcf
 
+# ok files seem to be off
+# what if I look at my poprob output?
+
 
