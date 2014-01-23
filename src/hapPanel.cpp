b7b8d4a0ea86c09fccb9c8fb84988c3925961e3f
kretzsch <wkretzsch@gmail.com>
2014-01-23 11:15:28 +0000

    git-annex automatic sync
    Former-commit-id: 7568aad1ec9421c4ae6bade97212ac153c46b0f1

---
+++ src/hapPanel.cpp
@@ -0,0 +1,51 @@
+#include "hapPanel.hpp"
+
+// require c++11
+static_assert(__cplusplus > 199711L, "Program requires C++11 capable compiler");
+
+using namespace std;
+
+void HapPanel::Init(vector<vector<char>> & inHaps){
+
+    assert(m_initialized == false);
+    m_numSites = inHaps.size();
+    assert(m_numSites > 0);
+    assert(inHaps[0].size() > 0);
+    assert(m_IDs.size() > 0);
+    m_numHaps = inHaps[0].size();
+    m_numWordsPerHap = static_cast<unsigned>(ceil(static_cast<double>(inHaps.size()) / m_wordSize));
+    vector< uint64_t > storeHaps = Char2BitVec(inHaps, m_numWordsPerHap, m_wordSize);
+    std::swap(m_haps, storeHaps);
+
+    assert(m_haps.size() % m_numHaps == 0);
+    m_initialized = true;
+
+}
+
+
+vector< uint64_t > HapPanel::Char2BitVec(const vector<vector<char> > & inHaps,
+                                         unsigned numWords, unsigned wordSize)
+{
+
+    assert(ceil(static_cast<double>(inHaps.size()) / wordSize) == numWords);
+
+    unsigned numSites = inHaps.size();
+    unsigned numHaps = inHaps[0].size();
+    vector<uint64_t> storeHaps;
+    storeHaps.resize(numWords * numHaps);
+
+    for (unsigned siteNum = 0; siteNum < numSites; siteNum++) {
+        for (unsigned hapNum = 0; hapNum < numHaps; hapNum ++) {
+            if (inHaps[siteNum][hapNum] == 0)
+                set0(&storeHaps[hapNum * numWords], siteNum);
+            else if (inHaps[siteNum][hapNum] == 1)
+                set1(&storeHaps[hapNum * numWords], siteNum);
+            else {
+                cout << "programming error";
+                throw 1;
+            }
+        }
+    }
+    return storeHaps;
+
+}
---
+++ src/hapPanel.hpp
@@ -0,0 +1,79 @@
+/* @(#)hapPanel.hpp
+ */
+
+#ifndef _HAPPANEL_H
+#define _HAPPANEL_H 1
+
+static_assert(__cplusplus > 199711L, "Program requires C++11 capable compiler");
+
+#include <math.h>
+#include <vector>
+#include <stdint.h>
+#include <string>
+#include <cassert>
+#include <iostream>
+
+#define    WordShift    6
+#define    WordMod    63
+
+class HapPanel {
+
+private:
+    unsigned m_wordSize = 64;
+    std::vector<uint64_t> m_haps;
+    std::vector<unsigned> m_positions;
+    std::vector<std::string> m_IDs;
+    unsigned m_numSites = 0;
+    unsigned m_numHaps = 0;
+    unsigned m_numWordsPerHap;
+    bool m_initialized = false;
+
+    void set1(uint64_t *P, unsigned I) {
+        // I >> Uint64_TShift is moving along the array according to which uint64_t I is in
+        // e.g. I <= 63 is first uint64_t, and so forth in blocks of 64 bits
+        P[I >> WordShift] |= static_cast<uint64_t>(1) << (I & WordMod);
+    }
+
+    void set0(uint64_t *P, unsigned I) {
+        P[I >> WordShift] &= ~(static_cast<uint64_t>(1) << (I & WordMod));
+    }
+
+    
+public:
+    void Init(std::vector<std::vector<char>> & inHaps, std::vector<std::string> & IDs){
+        std::swap(m_IDs, IDs);
+        Init(inHaps);
+    };
+    
+    void Init(std::vector<std::vector<char>> & inHaps);
+    void swapIDs(std::vector<std::string> & IDs){std::swap(m_IDs, IDs);}
+    std::string GetID(unsigned idx){
+        assert(idx < m_IDs.size());
+        return m_IDs[idx];
+    }
+    unsigned NumHaps (){
+        assert(m_initialized);
+        return m_numHaps;
+    }
+    unsigned NumWordsPerHap (){
+        assert(m_initialized);
+        return m_numWordsPerHap;
+    }
+    std::vector<uint64_t> * Haplotypes(){
+        assert(m_initialized);
+        return &m_haps;
+    }
+    unsigned MaxSites(){
+        assert(Initialized());
+        return NumWordsPerHap() * m_wordSize;
+    }
+    unsigned NumSites(){
+        assert(m_initialized);
+        return m_numSites;
+    }
+    bool Initialized(){return m_initialized;}
+    std::vector< uint64_t > Char2BitVec(const std::vector<std::vector<char> > & inHaps,
+                                   unsigned numWords, unsigned wordSize);
+};
+
+#endif
