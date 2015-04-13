d8d34c556b83c3b22e6b286d7fdcd815fef07430
Warren W. Kretzschmar <wkretzsch@gmail.com>
2015-04-13 15:23:06 +0100

    Adds bio.cpp
    Former-commit-id: 900f73e55d8585277921f2f7a369d9ede48766f8

---
+++ src/bio.cpp
@@ -0,0 +1,25 @@
+#include "bio.hpp"
+
+using namespace std;
+namespace Bio {
+
+// this returns a vector filled with the first n_max_tokens
+std::vector<std::string> tokenize_partial(const std::string &str,
+                                          size_t n_max_tokens,
+                                          const std::string &search) {
+  std::vector<std::string> tokens;
+  tokens.reserve(n_max_tokens);
+  std::string::size_type p_last = str.find_first_not_of(search, 0);
+  std::string::size_type p_curr = str.find_first_of(search, p_last);
+
+  if (n_max_tokens > 1)
+    while ((std::string::npos != p_curr || std::string::npos != p_last) &&
+           tokens.size() + 1 != n_max_tokens) {
+      tokens.push_back(str.substr(p_last, p_curr - p_last));
+      p_last = str.find_first_not_of(search, p_curr);
+      p_curr = str.find_first_of(search, p_last);
+    }
+
+  return tokens;
+}
+}
