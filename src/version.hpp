4337a2cb2f8f7c0e1251ce1966b4d65e963f58c7
kretzsch <wkretzsch@gmail.com>
2015-04-13 23:21:06 +0100

    Bumps to version 1.6.0
    Former-commit-id: eff597df072fe7fec4dd59c0673bf5efda97f976

--- src/version.hpp
+++ src/version.hpp
@@ -9,8 +9,8 @@
 #define VERSION_STR(s) #s
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 4
-#define VERSION_REVISION 13b
+#define VERSION_MINOR 6
+#define VERSION_REVISION 0
 
 #endif /* _VERSION_H */
 
